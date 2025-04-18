cf9eaece655f598d4f72787531a34ebb61030394
Chandan U <chandanucodeid04@gmail.com>
2025-04-18 23:00:27 +0530

    Update prog5_Client.c

--- prog5_Client.c
+++ prog5_Client.c
@@ -5,59 +5,54 @@
 #include <arpa/inet.h>
 
 #define PORT 17000
-#define BUFFER_SIZE 1024
+#define WINDOW_SIZE 4
+#define FRAME_COUNT 10
 
 int main() {
-    int sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[BUFFER_SIZE];
-    int expected_frame = 0, received_frame;
-    socklen_t addr_size;
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+    int base = 0, next_frame = 0, ack;
 
     // Create socket
-    (sock = socket(AF_INET, SOCK_STREAM, 0));
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
     // Define server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind the socket
-    bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    // Listen for connections
-    listen(sock, 5);
-
-    printf("Waiting for sender connection...\n");
+    // Connect to receiver
+    connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    printf("Connected to receiver.\n");
     
-    addr_size = sizeof(client_addr);
-    client_sock = accept(sock, (struct sockaddr *)&client_addr, &addr_size);
-    printf("Sender connected!\n");
-
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        recv(client_sock, buffer, BUFFER_SIZE, 0);
-
-        if (strcmp(buffer, "exit") == 0) {
-            printf("Transmission complete. Closing connection.\n");
-            break;
+    while (base < FRAME_COUNT) {
+        // Send frames within window size
+        while (next_frame < base + WINDOW_SIZE && next_frame < FRAME_COUNT) {
+            sprintf(buffer, "Frame %d", next_frame);
+            send(sock, buffer, strlen(buffer), 0);
+            printf("Sent: %s\n", buffer);
+            next_frame++;
         }
 
-        // Extract frame number
-        sscanf(buffer, "Frame %d", &received_frame);
+        // Wait for ACK
+        recv(sock, &ack, sizeof(ack), 0);
+        printf("Received ACK for Frame %d\n", ack);
 
-        if (received_frame == expected_frame) {
-            printf("Received: %s\n", buffer);
-            expected_frame++;
+        // Update base only if we get ACK for newer frames
+        if (ack > base) {
+            base = ack;
         } else {
-            printf("Out-of-order frame received (Frame %d). Discarding...\n", received_frame);
+            // Go-Back-N: Retransmit from base
+            printf("Timeout! Retransmitting from Frame %d\n", base);
+            next_frame = base; // Reset to retransmit window
         }
-
-        // Send ACK for the last in-order received frame
-        send(client_sock, &expected_frame, sizeof(expected_frame), 0);
     }
 
-    close(client_sock);
+    // Send termination message
+    strcpy(buffer, "exit");
+    send(sock, buffer, strlen(buffer), 0);
+    
     close(sock);
     return 0;
 }
