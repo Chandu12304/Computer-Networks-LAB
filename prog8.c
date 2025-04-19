3a9a1ab2f423aee1805a1ca8e01f1735aa3a8124
Chandan U <chandanucodeid04@gmail.com>
2025-04-19 23:33:23 +0530

    Create prog8.c

---
+++ prog8.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <arpa/inet.h> 
+
+int main() { 
+char host[100]; 
+
+printf("Enter domain: "); 
+scanf("%s", host); 
+
+struct hostent *h = gethostbyname(host); 
+
+if (h)  printf("IP: %s\n", inet_ntoa((struct in_addr)h->h_addr_list[0])); 
+else  printf("Lookup failed\n"); 
+
+return 0; 
+}
