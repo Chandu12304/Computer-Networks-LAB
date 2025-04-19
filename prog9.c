d47bdcd19431d0d4257b47a78205fe405947a576
Chandan U <chandanucodeid04@gmail.com>
2025-04-19 23:37:33 +0530

    Update prog9.c

--- prog9.c
+++ prog9.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
+void main(){
+	int in,out,bkt,n,stord=0,time=0;
+	printf("Enter the size of bucket: ");
+	scanf("%d",&bkt);
+	printf("Enter the output rate: ");
+	scanf("%d",&out);
+	printf("Enter the number of packets: ");
+	scanf("%d",&n);
+	while(1){
+		if(n==0 && stord==0)break;
+		if(stord==0){
+			printf("Enter the size of incoming packet: ");
+			scanf("%d",&in);
+			n--;
+				if(in>bkt){
+				printf("Packet rejected since packet size is greater than bucket size\n");
+				continue;
+			}
+			stord = in;
 
+		}
+		time++;
+		printf("---------------------------------------\n");
+		printf("\t\t\tAt time %d\n",time);
+
+		if(stord<out){
+			printf("Packet of size %d delivered\n",stord);
+			printf("---------------------------------------\n");
+			stord=0;
+			continue;
+		}
+		printf("Packet of size %d delivered\n",out);
+		printf("---------------------------------------\n");
+		stord-=out;
+
+
+	}
+}
