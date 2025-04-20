92769e47eec9159d827dcac96865895634fa5b89
Chandan U <chandanucodeid04@gmail.com>
2025-04-20 13:26:06 +0530

    Update prog7_Client.c

--- prog7_Client.c
+++ prog7_Client.c
@@ -1,39 +1,30 @@
-/* udp_client.c */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include <arpa/inet.h>
-#include <fcntl.h>  
+#include <unistd.h>
+#include <sys/stat.h>
+#include <netdb.h>
 
-#define BUF_SIZE 1024
-
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <server_ip> <port> <filename>\n", argv[0]);
-        return 1;
-    }
-
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in srv = {
-        .sin_family = AF_INET,
-        .sin_port = htons(atoi(argv[2]))
-    };
-    inet_pton(AF_INET, argv[1], &srv.sin_addr);
-
-int fd = open(argv[3], O_RDONLY);
-if (fd < 0) {
-    perror("open");
-    return 1;
-}
-
-char buf[BUF_SIZE];
-ssize_t n;
-while ((n = read(fd, buf, BUF_SIZE)) > 0) {
-    sendto(sock, buf, n, 0, (struct sockaddr*)&srv, sizeof(srv));
-}
-
-close(fd);
-
-    close(sock);
-    return 0;
+int main(int argc,char *argv[]){
+  if(argc<4){
+    printf("argument should contain 4 parameters");
+  }
+  int soc=socket(AF_INET,SOCK_DGRAM,0);
+  struct sockaddr_in addr;
+  addr.sin_family=AF_INET;
+  addr.sin_addr.s_addr=inet_addr(argv[1]);
+  addr.sin_port=htons(atoi(argv[2]));
+  
+  char buffer[1024];
+  
+  printf("\nReading from the file %s...\n",argv[3]);
+  int fd=open(argv[3],O_RDONLY);
+  int n= read(fd,buffer,sizeof(buffer));
+  
+  sendto(soc,buffer,sizeof(buffer),0,(struct sockaddr*)&addr,sizeof(addr));
+  printf("\nMessage is sent to server...\n");
+  
+  return 0;
 }
