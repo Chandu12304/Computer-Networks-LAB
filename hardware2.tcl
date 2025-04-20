13f44c64f6a06406a79dbbc93f932fdc15217f56
Chandan U <chandanucodeid04@gmail.com>
2025-04-20 22:21:02 +0530

    Update hardware2.tcl

--- hardware2.tcl
+++ hardware2.tcl
@@ -71,3 +71,19 @@ $ns at 0.1 "$ftp0 start"
 $ns at 4 "finish"
 
 $ns run
+
+
+# BEGIN{
+# tcp=0
+# udp=0
+# }
+# {
+# if($1=="r"&&$3=="2"&&$4=="3"&&$5=="tcp")
+# tcp++;
+# if($1=="r"&&$3=="2"&&$4=="3"&&$5=="cbr")
+# udp++;
+# }
+# END{
+# printf("The total number of packets by tcp is %d\n",tcp);
+# printf("The total number of packets by udp is %d\n",udp);
+# }
