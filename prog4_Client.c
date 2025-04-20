e76f70b57537977016ef1e0b7b23781bc7ca78ad
Chandan U <chandanucodeid04@gmail.com>
2025-04-20 19:14:09 +0530

    Update prog4_Client.c

--- prog4_Client.c
+++ prog4_Client.c
@@ -9,19 +9,12 @@ int main()
     char buffer[1024], fname[50];
     struct sockaddr_in addr;
 
-    /*  socket creates an endpoint for communication and returns a file descriptor */
-    soc = socket(AF_INET, SOCK_STREAM, 0);
-
-    /*
-     * sockaddr_in is used for ip manipulation
-     * we define the port and IP for the connection.
-     */
     addr.sin_family = AF_INET;
     addr.sin_port = htons(7891);
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    /*  keep trying to esatablish connection with server */
-    while(connect(soc, (struct sockaddr *) &addr, sizeof(addr))) ;
+    
+    connect(soc, (struct sockaddr *) &addr, sizeof(addr));
     printf("\nClient is connected to Server");
     printf("\nEnter file name: ");
     scanf("%s", fname);
@@ -30,7 +23,7 @@ int main()
 
     printf("\nRecieved response\n");
     /*  keep printing any data received from the server */
-    while ((n = recv(soc, buffer, sizeof(buffer), 0)) > 0)
+    n = recv(soc, buffer, sizeof(buffer), 0);
         printf("%s", buffer);
 
     return 0;
